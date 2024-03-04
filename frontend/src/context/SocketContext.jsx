15c11354a8ae10f20792fb4ba8494231dbef1fdf
BestOlumese <bestolumese@gmail.com>
2024-03-04 23:50:48 +0100

    patch

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chat-app-prod-hoth.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
